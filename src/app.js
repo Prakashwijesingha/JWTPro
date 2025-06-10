c28583feaa60947769ddfa13728e5bbdfc37c427
Prakash <prakashwijesinghanew@gmail.com>
2025-06-10 15:33:10 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const Express = require("express");
+
+const app = Express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on: http://localhost:${PORT}`);
+})
