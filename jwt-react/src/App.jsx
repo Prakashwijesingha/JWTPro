f5e728286d241be96a838e8e11724017cbdc0474
Prakash <prakashwijesinghanew@gmail.com>
2025-06-14 19:26:22 +0530

    update

--- jwt-react/src/App.jsx
+++ jwt-react/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="App flex flex-row items-center justify-center min-h-screen bgcover:bg-gray-100">
-      <h1 className="text-center textaling-center">React Application</h1>
+    <div className="App">
+      <h1 className=" text-center ">React Application</h1>
     </div>
 
   );
